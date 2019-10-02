e50116c541fe5ca2561e6c8b792d3de8fbae133c
Jenn Soderborg <jsoderborg7@gmail.com>
2019-10-01 23:26:15 -0600

    MVP complete

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const postsRouter = require('./posts-router');
+
 const db = require('./data/db');
 
 const server = express();
@@ -9,3 +11,8 @@ server.listen(port, () => console.log(`\n**Server listening on port ${port}**\n`
 
 server.use(express.json());
 
+server.use('/api/posts', postsRouter);
+
+server.get('/', (req, res) =>{
+  res.json('Yay! I am working!');
+});
